282fcc45bd2cfaa84fceebe63f1849978a72e9f7
Mozart409 <maderamadeus@gmail.com>
2022-05-30 10:00:53 +0200

    feat: :zap: added bg image from cloudflare

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -92,7 +92,6 @@ export let links: LinksFunction = () => {
 }
 
 export const loader: LoaderFunction = ({ request }) => {
-  console.log(request.headers)
   const lang = request.headers.get('cf-ipcountry')
 
   let country = countries.find((c) => c.cca2 === lang)
@@ -101,8 +100,6 @@ export const loader: LoaderFunction = ({ request }) => {
   /* if (cf.city) formattedLocation += cf.city + ', '
   if (cf.region) formattedLocation += cf.region + ', '
   formattedLocation += cf.country */
-
-  console.log('country ', country)
 
   return json({
     formattedLocation,
@@ -126,93 +123,7 @@ export default function Index() {
                   alt="Mozart409 Logo"
                 />
               </div>
-              {/* <div className="-mr-2 -my-2 md:hidden">
-                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-yellow-500">
-                  <span className="sr-only">Open menu</span>
-                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                </Popover.Button>
-              </div>
-              <Popover.Group as="nav" className="hidden md:flex space-x-10">
-                <Popover className="relative">
-                  {({ open }) => (
-                    <>
-                      <Popover.Button
-                        className={classNames(
-                          open ? 'text-gray-900' : 'text-gray-500',
-                          'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500'
-                        )}
-                      >
-                        <span>Solutions</span>
-                        <ChevronDownIcon
-                          className={classNames(
-                            open ? 'text-gray-600' : 'text-gray-400',
-                            'ml-2 h-5 w-5 group-hover:text-gray-500'
-                          )}
-                          aria-hidden="true"
-                        />
-                      </Popover.Button>
 
-                      <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-200"
-                        enterFrom="opacity-0 translate-y-1"
-                        enterTo="opacity-100 translate-y-0"
-                        leave="transition ease-in duration-150"
-                        leaveFrom="opacity-100 translate-y-0"
-                        leaveTo="opacity-0 translate-y-1"
-                      >
-                        <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform w-screen max-w-md lg:max-w-2xl lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
-                          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                            <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2">
-                              {solutions.map((item) => (
-                                <a
-                                  key={item.name}
-                                  href={item.href}
-                                  className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
-                                >
-                                  <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-gradient-to-r from-pink-600 to-yellow-500 text-white sm:h-12 sm:w-12">
-                                    <item.icon
-                                      className="h-6 w-6"
-                                      aria-hidden="true"
-                                    />
-                                  </div>
-                                  <div className="ml-4">
-                                    <p className="text-base font-medium text-gray-900">
-                                      {item.name}
-                                    </p>
-                                    <p className="mt-1 text-sm text-gray-500">
-                                      {item.description}
-                                    </p>
-                                  </div>
-                                </a>
-                              ))}
-                            </div>
-                          </div>
-                        </Popover.Panel>
-                      </Transition>
-                    </>
-                  )}
-                </Popover>
-
-                <a
-                  href="#"
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
-                >
-                  Pricing
-                </a>
-                <a
-                  href="#"
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
-                >
-                  Partners
-                </a>
-                <a
-                  href="#"
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
-                >
-                  Company
-                </a>
-              </Popover.Group>*/}
               <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
                 <span className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                   Amadeus Mader
@@ -237,8 +148,8 @@ export default function Index() {
                 <div className="absolute inset-0">
                   <img
                     className="h-full w-full object-cover"
-                    src="https://images.unsplash.com/photo-1616542986877-e6b9d0680a94?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
-                    alt="People working on laptops"
+                    src="https://imagedelivery.net/Xf-xzwkqsT6N08RkH1GoLQ/ff6e369d-23c1-48b4-48b9-4f4bbf47c400/public"
+                    alt="Munich Background"
                   />
                   <div className="absolute inset-0 bg-gradient-to-r from-pink-800 to-yellow-600 mix-blend-multiply" />
                 </div>
