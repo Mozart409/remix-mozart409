3f72b593559882abd64605de9bd679fcfa12ad05
Mozart409 <maderamadeus@gmail.com>
2022-06-08 11:42:33 +0200

    fix: :lock: added more csp values

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -12,6 +12,19 @@ export default function handleRequest(
     <RemixServer context={remixContext} url={request.url} />
   )
 
+  const headers = [
+    { 'Content-Security-Policy': 'upgrade-insecure-requests' },
+    { 'Strict-Transport-Security': 'max-age=7200' },
+    { 'X-Xss-Protection': '1; mode=block' },
+    { 'X-Frame-Options': 'DENY' },
+    { 'X-Content-Type-Options': 'nosniff' },
+    { 'Referrer-Policy': 'strict-origin-when-cross-origin' },
+    {
+      'Permissions-Policy':
+        'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), microphone=(), midi=(), usb=(), conversion-measurement=(), focus-without-user-activation=(), hid=(), idle-detection=(), interest-cohort=(), serial=(), sync-script=(), trust-token-redemption=(), window-placement=(), vertical-scroll=()',
+    },
+  ]
+
   responseHeaders.set('Content-Type', 'text/html')
 
   /* if (process.env.NODE_ENV === 'development') {
@@ -20,8 +33,11 @@ export default function handleRequest(
   if (process.env.NODE_ENV === 'production') {
     responseHeaders.set(
       'Content-Security-Policy',
-      "default-src 'self'; img-src 'self' https://imagedelivery.net; style-src 'self';"
+      "default-src 'self'  https://mozart409.com; img-src 'self' https://imagedelivery.net; style-src 'self';"
     )
+    headers.map((item) => {
+      return responseHeaders.set(Object.keys(item)[0], Object.values(item)[0])
+    })
   }
 
   return new Response('<!DOCTYPE html>' + markup, {
