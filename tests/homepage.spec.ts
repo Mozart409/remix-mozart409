8f261cdea66119c83d85fc1ce160e98330d56cdf
Amadeus Mader <amadeus@mozart409.com>
2025-12-04 22:17:14 +0100

    feat: add playwright

---
+++ tests/homepage.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Mozart409/);
+});
+
+test("has social links", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  // getByRole('link', { name: 'Twitter / X' });
+
+  await expect(
+    page.getByRole("main").getByRole("link", { name: "GitHub" }),
+  ).toBeVisible();
+  await expect(
+    page.getByRole("main").getByRole("link", { name: "Twitter / X" }),
+  ).toBeVisible();
+});
+
+
+test("has experience", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  await expect(
+   page.getByTestId("text-on-picture", {text: "Rust"}));
+});
